307592f6c107d6b7af2e63bd2b47cd01d8647516
Yullia90 <y.tsarenko@harwind.com.ua>
2024-02-25 14:37:49 +0200

    add favorite page

---
+++ src/pages/Favorite/Favorite.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Container, FilteredContainer } from './Favorite.styled';
+import { UseSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import { useFilter } from '../../utils';
+import { FilteredForm } from 'components/FilteredForm/FilteredForm';
+import { AdvertList } from 'components/AdvertList/AdvertList';
+import { AdvertsItems } from 'components/AdvertsItems/AdvertsItems';
+import { NoResultsFavorites } from 'components/NoResultsFavorites/NoResultsFavorites';
+
+const Favorite = () => { 
+    const favorites = useSelector((state) => state.favorites.favorites);
+
+    const initialFilters = {
+        make: '',
+        filteredPrices: [],
+        minMileage: '',
+        maxMileage: '',	
+    };
+
+    const { filters, filteredAdverts, isFiltering, handleFilterChange, handleResetClick, } = useFilter(favorites, initialFilters);
+    
+    const makes = [...new Set(favorites.map((advert) => advert.make))];
+    const prices = [...new Set(favorites.map((advert) => advert.rentalPrice.replace('$', ''))),];
+    const mileage = [...new Set(favorites.map((advert) => advert.mileage))];
+    const minMileage = Math.min(...mileage);
+    const maxMileage = Math.max(...mileage);
+
+    return (
+        <Container>
+            <FilteredContainer>
+            <FilteredForm
+                makes={makes}
+                prices={prices}
+                minMileage={minMileage}
+                maxMileage={maxMileage}
+                filters={filters}
+                onFilterChange={handleFilterChange}
+                onResetClick={handleResetClick}
+                />
+            </FilteredContainer>
+             {favorites.length === 0 ? (
+        <NoResultsFavorites />
+      ) : (
+        <>
+          {isFiltering ? (
+            filteredAdverts && filteredAdverts.length > 0 ? (
+              <AdvertsList filteredAdverts={filteredAdverts} />
+            ) : (
+              <NoResultsFavorites />
+            )
+          ) : (
+            favorites.map((advert, index) => (
+              <div key={advert.id}>
+                <AdvertsItems advert={advert} index={index} />
+              </div>
+            ))
+          )}
+        </>
+      )}
+      <ToastContainer />
+    </Container>
+  );
+};
+
+export default Favorites;
---
+++ src/pages/Favorite/Favorite.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  min-height: 65vh;
+  grid-gap: 50px;
+  padding-bottom: 20px;
+  max-width: 1184px;
+  margin: 0 auto;
+  margin-bottom: 50px;
+`;
+
+export const FilteredContainer = styled.div`
+  margin: 0 auto;
+`;
